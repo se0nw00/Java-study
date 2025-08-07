7716af9e698c61ab1f60d13f7e3f866f9bb1f96f
seonwoo <younseonwoo17@gmail.com>
2025-08-07 11:25:13 +0900

    Java practice

---
+++ src/first/OperatorExam.java
@@ -0,0 +1,34 @@
+package first;
+
+public class OperatorExam {
+
+	public static void main(String[] args) {
+		int i1 = -5;
+		int i2 = +i1;
+		int i3 = -i1;
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+		
+		int i4 = ++i3; // i3 = i3 + 1;
+		System.out.println(i4);
+		System.out.println(i3);
+		
+		int i5 = i3++; // i3 = i3 + 1;
+		System.out.println(i5);
+		System.out.println(i3);
+
+		int i = 5;
+		int j = 2;
+		
+		System.out.println(i + j);
+		System.out.println(i - j);
+		System.out.println(i * j);
+		System.out.println(i / (double)j);
+		System.out.println(i % j);
+
+
+	}
+
+}
---
+++ src/first/TypeCastingExam.java
@@ -0,0 +1,16 @@
+package first;
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+		int x = 50000;
+		long y = x;
+		
+		long x2 = 5;
+		int y2 =(int) x2;
+		
+		System.out.println(y);
+		System.out.println(y2);
+	}
+
+}
